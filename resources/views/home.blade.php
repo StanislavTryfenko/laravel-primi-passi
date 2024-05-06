dc5f98d347ca9e0174d9ebe98ef58f6a1370dccd
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-05-06 16:58:07 +0200

    Consegna

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="dark:bg-gray-900">
+    <div class="container mx-auto text-white flex place-content-center mt-10 text-3xl">
+        <h1>{{$welcome}}</h1>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $welcome = 'Hello Laravel';
+    
+    return view('home', compact('welcome'));
 });
